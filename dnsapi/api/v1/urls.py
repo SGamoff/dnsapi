5d9d3b5f6c8535d5bc0b6541dfb8797c072c7697
Sergey Gamov <sergey.gamov@alpeinsoft.securium.ch>
2018-11-06 01:59:14 +0300

    Change api call by name to api call by methods

--- dnsapi/api/v1/urls.py
+++ dnsapi/api/v1/urls.py
@@ -1,5 +1,6 @@
 from api.v1.views.management import *
 from django.urls import path
+from rest_framework import routers
 
 app_name = 'v1'
 
--- dnsapi/api/v1/views/management.py
+++ dnsapi/api/v1/views/management.py
@@ -7,6 +7,7 @@ from datetime import date
 from dns.exception import DNSException
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
+from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
